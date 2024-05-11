e97748f63253dc4f8fa5ced46a01378789d45fa2
jormundgandr <business.nimh@outlook.com>
2024-05-10 21:08:31 -0500

    creacion de login y token

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,12 @@
 const express = require('express');
 
 const usersRouter = require('./users.router');
-
+const userAuth = require ('./usersAuth.router')
 
 function routerApi(app) {
   const router = express.Router();
   app.use('/users', usersRouter);
+  app.use('/login', userAuth)
 }
 
 module.exports = routerApi;
---
+++ routes/usersAuth.router.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const router = express.Router();
+const User = require('../models/user');
+const jwt = require('jsonwebtoken');
+const secretKey = 'miClaveSecreta'; 
+const bcrypt = require('bcrypt');
+
+//Login
+router.post('/', async (req, res)=> {
+    const { email, password } = req.body;
+
+    // Validar datos de entrada
+    if (!email || !password) {
+      return res.status(400).json({ message: 'Some credetials are missing' });
+    }
+  
+    try {
+      // Buscar usuario por nombre de usuario
+      const user = await User.findOne({ email });
+      if (!user) {
+        return res.status(401).json({ message: 'wrong email and/or password' });
+      }
+  
+      // Comparar contraseña ingresada con la hasheada
+      const isMatch = await bcrypt.compare(password, user.password);
+      if (!isMatch) {
+        return res.status(401).json({ message: 'wrong email and/or password' });
+      }
+      // Generar token JWT
+      const token = jwt.sign({ userId: user._id }, secretKey);
+  
+      res.json({ message: 'Inicio de sesión exitoso', token });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: 'Error al iniciar sesión' });
+    }
+})
+
+module.exports = router;
