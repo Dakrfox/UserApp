ad709ff3bdd20f5d10233be0605de906444c5efe
jormundgandr <business.nimh@outlook.com>
2024-05-26 09:18:41 -0500

    fixing bugs and cors

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const routerApi = require('./routes/index');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 // Creamos una aplicación Express
 const app = express();
@@ -14,7 +15,9 @@ mongoose.connect('mongodb://root:password@localhost:27017/', {
 .then(() => console.log('Conectado a la base de datos'))
 .catch(err => console.error(err));
 
-
+app.use(cors({
+  origin: 'http://localhost:3001' // reemplaza esto con la URL de tu aplicación React
+}));
 // Definimos una ruta GET para la raíz '/'
 app.get('/', (req, res) => {
   // Enviamos una respuesta "Hola Mundo!" a la solicitud
