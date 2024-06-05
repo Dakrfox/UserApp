52f2d9e53bbccf92eba8e0de6fe8850887825628
jormundgandr <business.nimh@outlook.com>
2024-06-05 16:16:01 -0500

    fixing routes

--- routes/users.router.js
+++ routes/users.router.js
@@ -33,7 +33,7 @@ router.post('/', async (req, res) => {
       .then(() => console.log('Usuario creado'))
       .catch(err => console.error(err));
 
-    res.status(201).json({ message: 'User created successfully', newuser: user });
+    res.status(200).json({ message: 'User created successfully'});
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: 'Internal server error' });
